2b2b1577ebdff95c8bed8dc0eaafd893e80a5fa1
Michael Mottola <mottola64@gmail.com>
2020-05-11 18:24:46 -0400

    working on definition api call

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 class Definition extends React.Component {
-    render() {
-        return(
-            <div>Definition</div>
-        )
-    }
+  fetchDefinition = () => {
+    fetch(
+      `https://api.wordnik.com/v4/word.json/${this.state.word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=${process.env.REACT_APP_API_KEY_WORDNIK}`
+    )
+    .then((response) => response.json())
+      .then((response) => {
+        return response;
+      })
+      .then((response) => {
+        this.setState({
+          definition: response.definition,
+          results: response.results,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+  return <div>{this.state.definition}</div>;
+  }
 }
 
-export default Definition
+export default Definition;
--- src/components/RandomWord.js
+++ src/components/RandomWord.js
@@ -1,5 +1,6 @@
 //http://api.wordnik.com/v4/words.json/randomWord?api_key=YOURKEYHERE
 import React from "react";
+import Definition from './Definition'
 import { Jumbotron } from "reactstrap";
 
 class RandomWord extends React.Component {
@@ -10,12 +11,8 @@ class RandomWord extends React.Component {
   }
 
   fetchRandomWord = () => {
-    // https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=YOURAPIKEY
     fetch(
       `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${process.env.REACT_APP_API_KEY_WORDNIK}`,
-      {
-        method: "GET",
-      }
     )
       .then((response) => response.json())
       .then((response) => {
@@ -38,7 +35,7 @@ class RandomWord extends React.Component {
         <Jumbotron>
           <h1 className="display-3">Random Word</h1>
           <ul>{this.state.word}</ul>
-          <ul></ul>
+          <Definition word={this.state.word}/>
         </Jumbotron>
       </div>
     );
