6ca83000d9d9936fd77c49476d9a2bac5c05300b
Michael Mottola <mottola64@gmail.com>
2020-05-11 17:54:28 -0400

    definition

---
+++ src/components/Definition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Definition extends React.Component {
+    render() {
+        return(
+            <div>Definition</div>
+        )
+    }
+}
+
+export default Definition
